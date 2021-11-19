65166baf6d8c6b8bb3d1b78b99e6fd397190d866
juliajulia89 <julkajanicka@gmail.com>
2021-11-19 17:38:00 +0100

    push

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ const Api = require("../apis/api");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  User.find().then((users) => res.render("index", { title: "Coffee", users }));
+  User.find().then((users) =>
+    res.render("index", { title: "Coffee Heaven", users })
+  );
 });
 
 /* GET from API */
 router.get("/api", (req, res) => {
   Api.getAll().then((entity) =>
-    res.render("index", { title: "Coffee", users: entity })
+    res.render("index", { title: "Coffee Heaven", users: entity })
   );
 });
 
