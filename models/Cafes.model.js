04a5cca041af770ba97730b624a672fdec96da8f
Greg Munro <gregmunro90@gmail.com>
2021-11-22 12:06:54 +0100

    cafe models updated

--- models/Cafes.model.js
+++ models/Cafes.model.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const cafeSchema = new Schema({
-  name: { type: String },
-  address: String,
-  priceLevel: { type: String },
-  image: String,
+  name: {type: String, unique: true, required: true}, 
+  address: {type: String, unique: true, required: true},
+  priceLevel: {type: String, required: true},
+  image: {type: String, default: "https://media.istockphoto.com/photos/coffee-shop-picture-id1279333349"},//supplies default img if nothing provided, use cloudinary to upload imgs from user
+  beans: [{type: Schema.Types.ObjectId, ref: 'Bean'}],
   //review:[{
   //         user: String,
   //       comments: String]}
 });
 
-// const Book = model('Book', bookSchema);
-// module.exports = Book;
 
 module.exports = model("Cafes", cafeSchema);
