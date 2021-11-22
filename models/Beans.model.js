6d1a0cbbc9bedf7d371832e1f6d4e2ec4bfb1a04
juliajulia89 <julkajanicka@gmail.com>
2021-11-22 09:34:35 +0100

    delete farmer

--- models/Beans.model.js
+++ models/Beans.model.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const beanSchema = new Schema(
-  {
-    name: String ,
-    origin: String, 
-    farmer: String,
-    flavorProfile: String,
-    cafes:  { type: Schema.Types.ObjectId, ref: "Cafes" },
-    image: String
-  },
-
-);
+const beanSchema = new Schema({
+  name: String,
+  origin: String,
+  flavorProfile: String,
+  cafes: { type: Schema.Types.ObjectId, ref: "Cafes" },
+  image: String,
+});
 
 // const Book = model('Book', bookSchema);
 // module.exports = Book;
