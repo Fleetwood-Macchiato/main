10e60694f2ac4e9ef59f149a5920cf4b920f18eb
juliajulia89 <julkajanicka@gmail.com>
2021-11-22 20:20:00 +0100

    no commet

--- models/Beans.model.js
+++ models/Beans.model.js
@@ -6,6 +6,7 @@ const beanSchema = new Schema({
   origin: String,
   flavorProfile: String,
   image: String,
+  cafe: [{ type: Schema.Types.ObjectId, ref: "Cafe" }],
 });
 
 module.exports = model("Bean", beanSchema);
