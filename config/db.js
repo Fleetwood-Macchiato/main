fcefa657f90e7e598a1151a9bb6dfb0e460496df
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-11-22 11:41:02 +0100

    login/signup

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-const Cafe = require('../models/Cafes.model')
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
