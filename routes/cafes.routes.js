37f15ab9a259e5f0219cc5c4aee4b7086aa34114
colinchadwick1 <colinchadwick@hotmail.co.uk>
2021-11-23 09:18:02 +0100

    changes

--- routes/cafes.routes.js
+++ routes/cafes.routes.js
@@ -4,6 +4,30 @@ const router = express.Router();
 // ********* require Book model in order to use it *********
 const Cafe = require("../models/Cafes.model");
 
+router.get("/add-cafe", (req, res) => {
+  res.render("users/add-cafe");
+});
+
+router.post("/add-cafe", async (req, res) => {
+  try {
+    const { name, address, priceLevel, image, beans } = req.body;
+    const createdCafe = await Cafe.create({
+      name,
+      address,
+      priceLevel,
+      image,
+      beans,
+    });
+
+    console.log(createdCafe);
+    res.redirect("/cafes");
+  } catch (error) {
+    console.log(error);
+    res.render("/add-cafe");
+  }
+});
+
+
 router.get("/", async (req, res) => {
   try {
     let listCafes = await Cafe.find();
@@ -25,5 +49,7 @@ router.get("/cafe-details", async (req, res) => {
     (err) => console.log(err);
   }
 });
+
+//add cafes
 
 module.exports = router;
