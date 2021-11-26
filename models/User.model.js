6a31872584dbfa0a4bde3e8c2fa9bd305b866f3e
juliajulia89 <julkajanicka@gmail.com>
2021-11-26 10:43:31 +0100

    clean up

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,4 @@
 const { Schema, model } = require("mongoose");
-
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
@@ -16,16 +14,12 @@ const userSchema = new Schema({
     required: [true, "Email is required"],
     unique: true,
   },
-
   favorites: [{ type: Schema.Types.ObjectId, ref: "Cafes", default: [] }],
-  
   image: {
     type: String,
     default:
       "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
   }
 });
-
 const User = model("User", userSchema);
-
 module.exports = User;
